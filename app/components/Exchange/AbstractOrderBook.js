52711c61ecd4acb3c928bcd3dba4e3061ff6be06
VerevkinAlexander <verevkin.alex@apriorit.com>
2019-06-13 19:46:45 +0300

    added needed data calls

--- app/components/Exchange/AbstractOrderBook.js
+++ app/components/Exchange/AbstractOrderBook.js
@@ -1,16 +1,20 @@
 import MarketsActions from "actions/MarketsActions";
+import MarketsStore from "stores/MarketsStore";
 import {checkFeeStatusAsync} from "common/trxHelper";
 
-class AbstractOrderBook {
-    constructor(prefix) {
-        this._orderBook_prefix = prefix;
-    }
-}
-
-class RegularOrderBook extends AbstractOrderBook {
+class RegularOrderBook {
     constructor(props) {
         super(props);
+        this.activeMarketHistory = MarketsStore.getState().activeMarketHistory; //latest change class
+        this.marketLimitOrders = MarketsStore.getState().marketLimitOrders; //orders
+        this.marketCallOrders = MarketsStore.getState().marketCallOrders; //calls
+        this.invertedCalls = MarketsStore.getState().invertedCalls; //invertedCalls
+        this.marketData = MarketsStore.getState().marketData; //combinedBids combinedAsks highestBid lowestAsk groupedBids groupedAsks
+        this.totals = MarketsStore.getState().totals; //totalBids totalAsks
+        this.trackedGroupsConfig = MarketsStore.getState().trackedGroupsConfig;
+        this.currentGroupOrderLimit = MarketsStore.getState().currentGroupLimit;
     }
+
     createLimitOrder2(orders) {
         return MarketsActions.createLimitOrder2(orders);
     }
@@ -38,9 +42,19 @@ class RegularOrderBook extends AbstractOrderBook {
             type: "limit_order_create"
         });
     }
+    unSubscribeMarket(quoteAssetId, baseAssetId) {
+        return MarketsActions.unSubscribeMarket(quoteAssetId, baseAssetId);
+    }
+    SubscribeMarket(quoteAssetId, baseAssetId, newBucketSize) {
+        MarketsActions.subscribeMarket(
+            quoteAssetId,
+            baseAssetId,
+            newBucketSize
+        );
+    }
 }
 
-class HtlcOrderBook extends AbstractOrderBook {
+class HtlcOrderBook {
     constructor(props) {
         super(props);
     }
@@ -56,8 +70,8 @@ class HtlcOrderBook extends AbstractOrderBook {
 
 const _orderbookCache = {};
 const orderBook = prefix => {
-    _orderbookCache[prefix] =
-        prefix === "htlc" ? new HtlcOrderBook() : new RegularOrderBook();
+    return (_orderbookCache[prefix] =
+        prefix === "htlc" ? new HtlcOrderBook() : new RegularOrderBook());
 };
 
 export default orderBook;
